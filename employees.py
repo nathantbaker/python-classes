665d19564d41f3afc6362ebe4c1d8bfa8363dfb8
Nathan T. Baker <nathantbaker@gmail.com>
2017-01-09 13:46:33 -0600

    basic string about employee spits out

--- employees.py
+++ employees.py
@@ -1,14 +1,19 @@
 class Company(object):
     """This represents a company in which people work"""
 
-    def __init__(self, name, title, start_date):
+    def __init__(self, name):
         self.name = name
-        self.title = title
-        self.start_date = start_date
-
     def get_name(self):
         """Returns the name of the company"""
         return self.name
+
+    def add_name(self, name):
+        """Returns the name of the company"""
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
 
 class Employee(object):
 
@@ -24,7 +29,7 @@ class Employee(object):
         self.name = name
 
     def __str__(self):
-        return "I'm an employee named {}".format(self.name)
+        return "Hi, I'm {}, an employee at the company {}.".format(self.name, my_company)
 
 
 # Create a class that contains information about employees of a company and define methods to
@@ -42,6 +47,7 @@ class Employee(object):
 
 # Create a company, and three employees, and then assign the employees to the company.
 
+my_company = Company("Apple")
 bruce = Employee()
 bruce.add_name("Bruce")
 print(bruce)
